2c8e653bdaceb90d7429c389beea888f8e81cb6e
Shaarang Singh <shaarangg.singh@gmail.com>
2021-12-05 19:34:46 +0530

    feat: Bg screen

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Jumpy")
-
+bg_image = pygame.image.load('assets/bg.png').convert_alpha()
 run =True
 
 while run:
+    screen.blit(bg_image,(0,0))
+
+
+
+
+    # Event Handler
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+    pygame.display.update()
 pygame.quit()

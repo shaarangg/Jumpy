7323e6f410f0630bc909277e44a1af2d9774bbd9
Shaarang Singh <shaarangg.singh@gmail.com>
2021-12-05 18:07:14 +0530

    feat: Assets

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Jumpy")
+
+run =True
+
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+pygame.quit()

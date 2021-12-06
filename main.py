a04f8b7886ceac2f51c44ba5edfae1042f8130cc
Shaarang Singh <shaarangg.singh@gmail.com>
2021-12-06 16:38:58 +0530

    feat: Player movement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from os import close
 import pygame
+from pygame.display import set_palette
 
 pygame.init()
 
@@ -8,6 +10,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Jumpy")
 
+
+# Setting frame rate
+FPS = 60
+clock=pygame.time.Clock()
 
 # Define colours
 WHITE = (255,255,255)
@@ -25,9 +31,31 @@ class Player():
         self.height = 40
         self.rect = pygame.Rect(0,0,self.width, self.height)
         self.rect.center = (x,y)
+        self.flip = False
     
+    def move(self):
+        dx=0
+        dy=0
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+            self.flip=True
+            dx-=10
+        if key[pygame.K_d]:
+            self.flip=False
+            dx+=10
+
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+
+        if self.rect.right + dx > SCREEN_WIDTH:
+            dx = SCREEN_WIDTH - self.rect.right
+            
+        self.rect.x+=dx
+        self.rect.y+=dy
+
+
     def draw(self):
-        screen.blit(self.image, (self.rect.x - 10,self.rect.y - 5))   
+        screen.blit( pygame.transform.flip(self.image,self.flip,False), (self.rect.x - 10,self.rect.y - 5))   
         pygame.draw.rect(screen,WHITE,self.rect,2)
 
 jumpy = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT - 150)
@@ -35,8 +63,8 @@ jumpy = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT - 150)
 run =True
 while run:
     screen.blit(bg_image,(0,0))
-
-
+    clock.tick(FPS)
+    jumpy.move()
     jumpy.draw()
 
     # Event Handler

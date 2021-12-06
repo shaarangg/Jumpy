15f8d82697fb63ca286df3caef6ffd606bf861aa
Shaarang Singh <shaarangg.singh@gmail.com>
2021-12-06 19:26:30 +0530

    feat: Platforms

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from os import close
 import pygame
-from pygame.display import set_palette
+import random
 
 pygame.init()
 
@@ -18,7 +17,7 @@ clock=pygame.time.Clock()
 
 # Game Variables
 GRAVITY = 1
-
+MAX_PLATFORMS = 10
 
 # Define colours
 WHITE = (255,255,255)
@@ -26,6 +25,7 @@ WHITE = (255,255,255)
 # Load Assets
 bg_image = pygame.image.load('assets/bg.png').convert_alpha()
 player_image = pygame.image.load('assets/jump.png').convert_alpha()
+platform_image = pygame.image.load('assets/wood.png').convert_alpha()
 
 
 # Player class
@@ -54,16 +54,30 @@ class Player():
         self.vel_y += GRAVITY
         dy+=self.vel_y
 
+
+
+        # Colissions with boundary
         if self.rect.left + dx < 0:
             dx = -self.rect.left
 
         if self.rect.right + dx > SCREEN_WIDTH:
             dx = SCREEN_WIDTH - self.rect.right
 
+
+        # Colissions with platforms
+        for platform in platform_group:
+            if(platform.rect.colliderect(self.rect.x,self.rect.y +dy, self.width, self.height)):
+                if self.vel_y >0:
+                    self.rect.bottom = platform.rect.top
+                    dy=0
+                    self.vel_y = -20
+        # Collissions with ground
+        
         if self.rect.bottom + dy > SCREEN_HEIGHT:
             dy=0
             self.vel_y = -20
-            
+        
+
         self.rect.x+=dx
         self.rect.y+=dy
 
@@ -72,12 +86,37 @@ class Player():
         screen.blit( pygame.transform.flip(self.image,self.flip,False), (self.rect.x - 10,self.rect.y - 5))   
         pygame.draw.rect(screen,WHITE,self.rect,2)
 
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self,width,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(platform_image, (width,10))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+# Player instance
 jumpy = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT - 150)
+
+
+# Platform
+platform_group = pygame.sprite.Group()
+
+for p in range(MAX_PLATFORMS):
+    p_w = random.randint(40,60)
+    p_x = random.randint(0,SCREEN_WIDTH-p_w)
+    p_y = p*random.randint(80,120)
+    platform = Platform(p_w,p_x,p_y)
+    platform_group.add(platform)
 
 run =True
 while run:
     screen.blit(bg_image,(0,0))
     clock.tick(FPS)
+
+    platform_group.draw(screen)
+
     jumpy.move()
     jumpy.draw()
 
